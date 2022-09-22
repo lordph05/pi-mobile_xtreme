028cabcf4919ce039bc6bb3247a838a03cab3dc1
Paulo Henrique <ph.jesus@gmail.com>
2022-09-22 00:13:38 -0300

    Erro ao fazer login

--- app/src/main/java/com/example/controledeestoque_xtreme/DAO/UserDAO.java
+++ app/src/main/java/com/example/controledeestoque_xtreme/DAO/UserDAO.java
@@ -3,13 +3,20 @@ package com.example.controledeestoque_xtreme.DAO;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
 import com.example.controledeestoque_xtreme.Endidades.User;
+
+import java.util.List;
 
 @Dao
 public interface UserDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    public void insert (User usuarios);
+    public void insert (User user);
+
+@Query("select * from user WHERE email=:email AND senha=:senha")
+    public List <User> getUser(String email, String senha);
+
 
 
 }
--- app/src/main/java/com/example/controledeestoque_xtreme/autenticacao/LoginActivity.java
+++ app/src/main/java/com/example/controledeestoque_xtreme/autenticacao/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.controledeestoque_xtreme.autenticacao;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,8 +10,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.controledeestoque_xtreme.DAO.UserDAO;
+import com.example.controledeestoque_xtreme.Endidades.User;
+import com.example.controledeestoque_xtreme.MainActivity;
 import com.example.controledeestoque_xtreme.R;
+import com.example.controledeestoque_xtreme.Utils.BancoDeDados;
+
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +26,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     EditText edit_email, edit_senha;
     Button btn_login;
     TextView text_criar_conta, text_recuperar_conta;
+    BancoDeDados bd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +56,26 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         else if (origem.getId()==R.id.text_recuperar_conta){
             Intent intent = new Intent(LoginActivity.this, RecuperarContaActivity.class);
             startActivity(intent);
-        }else if (origem.getId()==R.id.btn_login){
-            Log.d("Click", "clicou no botao login");
+        }else if(origem.getId()==R.id.btn_login){
+            // capturar as informações de login e senha
+            String email = edit_email.getText().toString();
+            String senha = edit_senha.getText().toString();
+            // obter uma instancia do banco de dados
+            bd = Room.databaseBuilder(getApplicationContext(),BancoDeDados.class, "BancoApp").allowMainThreadQueries().build();
+            // obtem o DAO de User
+            UserDAO userDAO = bd.userDAO();
+            List<User> user = userDAO.getUser(email,senha);
+            if(user.size()==0){ // dados incorretos
+                Toast toast =  Toast.makeText (LoginActivity.this,"Dados do usuário incorretos!",Toast.LENGTH_SHORT);
+                toast.show();
+            }else{ // dados corretos (permissão concedida)
+                // mudar de tela
+                Toast toast =  Toast.makeText (LoginActivity.this,"Permissão concedid!",Toast.LENGTH_SHORT);
+                toast.show();
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+
         }
 
         }
