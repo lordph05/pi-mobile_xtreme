3aca08ce04c4369a1c74b19e9014cf5ea0a60f9a
Paulo Henrique <ph.jesus@gmail.com>
2022-11-08 23:23:58 -0300

    teste2

--- app/src/main/java/com/example/controledeestoque_xtreme/Utils/BancoDeDados.java
+++ app/src/main/java/com/example/controledeestoque_xtreme/Utils/BancoDeDados.java
@@ -8,7 +8,7 @@ import com.example.controledeestoque_xtreme.DAO.UserDAO;
 import com.example.controledeestoque_xtreme.Endidades.Produtos;
 import com.example.controledeestoque_xtreme.Endidades.User;
 
-//@Database(entities = {User.class, Produtos.class},version = 1)
+@Database(entities = {User.class, Produtos.class},version = 1)
 public abstract class BancoDeDados extends RoomDatabase {
     public abstract UserDAO getuserDAO();
     public abstract ProdutoDAO getProdutoDAO();
